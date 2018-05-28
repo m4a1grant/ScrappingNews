7248e549562364d53299915d8b36d380ab3d1d82
m4a1grant <m4a1grant75@gmail.com>
2018-05-28 14:39:30 +0300

    Version 1.0

--- scraper.py
+++ scraper.py
@@ -4,26 +4,41 @@ from bs4 import BeautifulSoup
 import csv
 
 BASE_URL = 'https://globalnews.ca/search/facebook/'
+
 #https://globalnews.ca/search/facebook/?post_type=any&orderby=&paged=6&ajax=true
 
 def get_html(url):
-    return requests.get(url).text
+    result = ''
+    for i in range(1, 11):
+        payload = {'post_type': 'any',
+                   'orderby': '',
+                   'paged': i,
+                   'ajax': 'true'}
+        result += requests.post(url, data=payload).text
+    return result
 
 
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
-    gnca_search_wrap = soup.find('div', id='gnca-search-wrap')
-    articles = gnca_search_wrap.find_all('article')
+    articles = soup.find_all('article')
     result = []
     for art in articles:
         data = {}
-        link = art.find('h3', class_='story-h').find('a')
-        data['link'] = link.get('href')
-        data['text'] = link.contents[0].strip()
-        dt = soup.find('h6', class_='story-date').contents[0].strip()
-        data['date'] = get_iso_date(dt)
+        try:
+            data['link'] = art.find('h3', class_='story-h').find('a').get('href')
+        except:
+            data['link'] = 'Can not find the link'
+        try:
+            data['text'] = art.find('h3', class_='story-h').find('a').text.strip()
+        except:
+            data['text'] = 'Can not find the link'
+        try:
+            dt = art.find('h6', class_='story-date').text.strip()
+            data['date'] = get_iso_date(dt)
+        except:
+            data['date'] = 'Can not find the date'
+
         result.append(data)
-    #print(result[0])
     add_to_csv(result)
 
 
